3318d3cfdef08c129dc42e9d572c4617e342a53f
yopandelreyz <cahyaadi679@gmail.com>
2025-12-05 21:43:10 +0700

    Create config.js

---
+++ pakasir/config.js
@@ -0,0 +1,18 @@
+module.exports = {
+  // Pakasir
+  PAKASIR_PROJECT: process.env.PAKASIR_PROJECT || "newproject",
+  PAKASIR_API_KEY: process.env.PAKASIR_API_KEY || "AsyIISrYJ67r9M8ZAAE33n5XvWaI9Fa9",
+  IS_SANDBOX: process.env.PAKASIR_SANDBOX === "true",
+
+  // Pterodactyl
+  PTERO_EGG: process.env.PTERO_EGG || "15",
+  PTERO_NEST_ID: process.env.PTERO_NEST_ID || "5",
+  PTERO_LOC: process.env.PTERO_LOC || "1",
+  PTERO_DOMAIN: process.env.PTERO_DOMAIN || "https://alpha-premier.privatserver.my.id",
+  PTERO_API_KEY: process.env.PTERO_API_KEY || "ptla_5oEDSwf1GFSvI5tQ4UU717QDy295AB071KVLPwTBpP2",
+  PTERO_CLIENT_KEY: process.env.PTERO_CLIENT_KEY || "ptlc_tw8O2cKsga7ym8pP3vDZe5K67Z9pifguRMNDOZ9bBcl",
+
+  // Gambar (opsional, untuk dokumentasi saja)
+  LOGO_MAIN: process.env.LOGO_MAIN || "https://files.catbox.moe/5rm670.bin",
+  PLAN_IMAGE_URL: process.env.PLAN_IMAGE_URL || "https://qu.ax/xbwtf"
+};
