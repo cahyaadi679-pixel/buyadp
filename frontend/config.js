de646e7d83092f52d955fca607e5a3a638e77894
yopandelreyz <cahyaadi679@gmail.com>
2025-12-05 21:46:21 +0700

    Create config.js

---
+++ frontend/config.js
@@ -0,0 +1,11 @@
+window.APP_CONFIG = {
+  apiBaseUrl: "/api",
+  currencyPrefix: "Rp",
+  currencySuffix: "/bulan",
+
+  // Gambar-gambar
+  mainLogoUrl: "https://files.catbox.moe/5rm670.bin",  // header & loading
+  planImageUrl: "https://qu.ax/xbwtf",                 // ikon di kartu panel 1GB–Unlimited
+  scriptImageUrl: "https://qu.ax/xbwtf"                // boleh sama, atau ganti kalau mau beda
+};
+
